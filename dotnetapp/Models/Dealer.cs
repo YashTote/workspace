b05919589cb2361f8cfd9627cbc96d6d8ffbfef9
Iamneo <techadmin@iamneo.ai>
2023-11-01 11:53:30 +0000

    Backup Commit

---
+++ dotnetapp/Models/Dealer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+namespace dotnetapp.Models
+{
+    public class Dealer
+    {
+       public int ID{get;set;}
+       public string Name{get;set;}
+       public DateTime RegistrationDate{get;set;}
+
+       public string AutoPartName{get;set;}
+       public string Manufacturer{get;set;}
+       public string MobileNumber{get;set;}
+       public string Email{get;set;}
+       public string description{get;set;} 
+    }
+}
