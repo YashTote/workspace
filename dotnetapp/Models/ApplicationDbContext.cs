a1d780053e77762d670eab229e7d678522969490
Iamneo <techadmin@iamneo.ai>
2023-11-01 12:08:30 +0000

    Backup Commit

---
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace dotnetapp.Models;
+
+public class ApplicationDbContext:DbContext
+{
+    public virtual DbSet<Dealer>Dealers{get;set;}
+
+    public ApplicationDbContext(){}
+    public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options):base(options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("User ID:sa;password=examlyMssql@123;server=localhost;Database=ApplicationDb;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+        }
+    }
+}
